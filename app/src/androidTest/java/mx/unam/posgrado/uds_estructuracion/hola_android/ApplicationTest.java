3b6429a59c1e61ea1e97cb4f086c53eae882135f
UDS_Estructuracion <guillermoab@posgrado.unam.mx>
2016-05-28 11:44:42 -0500

    Primera Versión

---
+++ app/src/androidTest/java/mx/unam/posgrado/uds_estructuracion/hola_android/ApplicationTest.java
@@ -0,0 +1,13 @@
+package mx.unam.posgrado.uds_estructuracion.hola_android;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
